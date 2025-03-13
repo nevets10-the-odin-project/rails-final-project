9cbd3cf9aa64b7a2317ce9404416310ad299af23
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-12 18:58:39 -0700

    Setup associations for user follow requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,10 @@ class User < ApplicationRecord
   has_many :comments, inverse_of: 'author', foreign_key: 'author_id'
   has_many :commented_posts, through: :comments, inverse_of: 'commenters'
   has_many :likes
-  has_many :liked_posts, through: 'likes', inverse_of: 'user_likes'
+  has_many :liked_posts, through: :likes, inverse_of: 'user_likes'
+  has_many :following_requests
+  has_many :follower_requests, through: :following_requests, inverse_of: 'follow_recipient',
+                               foreign_key: 'follow_recipient_id'
+  has_many :followee_requests, through: :following_requests, inverse_of: 'follow_submitter',
+                               foreign_key: 'follow_submitter_id'
 end
--- app/models/user_follow_request.rb
+++ app/models/user_follow_request.rb
@@ -1,2 +1,4 @@
 class UserFollowRequest < ApplicationRecord
+  belongs_to :follow_recipient, class_name: 'User'
+  belongs_to :follow_submitter, class_name: 'User'
 end
