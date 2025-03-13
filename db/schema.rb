fa7fcb763bb38cff65bd4302e67ec363c9787db8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-12 17:27:58 -0700

    Add and migrate username col to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_023802) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_13_002735) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_023802) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
