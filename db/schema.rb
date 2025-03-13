4300db06065c26a68ad6cac0241e4ccc3cb82c27
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-12 18:31:49 -0700

    Generate and migrate Like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_13_011652) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_13_013101) do
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
     t.integer "post_id", null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_13_011652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -43,5 +52,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_13_011652) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
