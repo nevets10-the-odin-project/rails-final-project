834447df73869f5930fa43828f15678ce3b9c377
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-14 13:21:58 -0700

    Add user's posts to user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ser_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = User.all
