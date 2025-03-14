aa2f0ce8ea7fa278927cbb9df411aa0cab2f7951
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-14 14:52:11 -0700

    Generate user likes and requests controllers

---
+++ app/helpers/user_follow_requests_helper.rb
@@ -0,0 +1,2 @@
+module UserFollowRequestsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, :users
   resources :comments, only: %i[new create update destroy]
-  resources :likes, only: %i[create destroy]
+  resources :likes, :user_follows, :user_follow_requests, only: %i[create destroy]
   root 'posts#index'
 end
